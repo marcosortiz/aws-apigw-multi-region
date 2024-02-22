5f07022c8d0ffc2e3b6348ceb7967365aabdac2d
marcosortiz <marcos.ortiz@icloud.com>
2024-02-22 15:12:11 +0000

    Adding service 2

--- test.sh
+++ test.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-url=https://service.example.com
+service1_url=https://service1.example.com
+service2_url=https://service2.example.com
 
 while true; do
   current_time=$(date +"%T")
   echo "[$current_time] Sending requests ..."
-  curl "$url"
+  curl "$service1_url"
+  echo -e ""
+  curl "$service2_url"
   echo -e ""
   echo -e "----------------------------------------"
-  sleep 5  
+  sleep 5
 done
