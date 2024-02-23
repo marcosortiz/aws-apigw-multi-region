f204f88790a82e83e33909e236da20fe4f9e0129
marcosortiz <marcos.ortiz@icloud.com>
2024-02-23 00:21:20 +0000

    adding external api url

--- test.sh
+++ test.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+external_api_url==https://externalapi.example.com
 service1_url=https://service1.example.com
 service2_url=https://service2.example.com
 
 while true; do
   current_time=$(date +"%T")
   echo "[$current_time] Sending requests ..."
+  curl "$external_api_url"
+  echo -e ""
   curl "$service1_url"
   echo -e ""
   curl "$service2_url"
